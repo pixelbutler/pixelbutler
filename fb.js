e726f29a99ac2caf2fa2a93d82717ec3a3f2fab6
Stephen Whitmore <stephen.whitmore@gmail.com>
2014-05-06 21:56:39 -0700

    Tons o functionality.

--- fb.js
+++ fb.js
@@ -1,137 +1,341 @@
 // TODO(sww): fancy module.exports jazz
 
-function framebuffer(width, height) {
-  // TODO(sww): Actually create + insert a canvas dynamically here.
-
-  this.px = new Array(width);
-  for(var i=0; i < width; i++) {
-    this.px[i] = new Array(height);
-      for(var j=0; j < height; j++) {
-        this.px[i][j] = [ Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256) ];
-      }
-  }
-  var px = this.px;
-
-  this.width = width;
-  this.height = height;
-
-  this.fillRect = function(x, y, w, h, col) {
-    x = Math.floor(x);
-    y = Math.floor(y);
-    w = Math.floor(w);
-    h = Math.floor(h);
-    for(var i=x; i < x+w; i++) {
-      for(var j=y; j < y+h; j++) {
-        if(i < 0 || j < 0 || i >= width || j >= height) {
-          continue;
-        }
-        px[i][j][0] = col[0];
-        px[i][j][1] = col[1];
-        px[i][j][2] = col[2];
-      }
-    }
+(function() {
+  /**
+   * Based off of a Gist of a 4x4 font by Martin Holzhauer:
+   * https://gist.github.com/woodworker/7696835
+   *
+   * Features fairly significant modifications to fit characters into smaller horizontal space.
+   */
+  var letters = {
+      "A":[
+          "1111",
+          "1001",
+          "1111",
+          "1001"
+      ],
+      "B": [
+          "100",
+          "111",
+          "101",
+          "111"
+      ],
+      "C": [
+          "1111",
+          "1000",
+          "1000",
+          "1111"
+      ],
+      "D": [
+          "1110",
+          "1001",
+          "1001",
+          "1110"
+      ],
+      "E": [
+          "111",
+          "110",
+          "100",
+          "111"
+      ],
+      "F": [
+          "111",
+          "100",
+          "110",
+          "100"
+      ],
+      "G": [
+          "111",
+          "100",
+          "101",
+          "111"
+      ],
+      "H": [
+          "101",
+          "101",
+          "111",
+          "101"
+      ],
+      "I": [
+          "1",
+          "1",
+          "1",
+          "1"
+      ],
+      "J": [
+          "001",
+          "001",
+          "101",
+          "111"
+      ],
+      "K": [
+          "101",
+          "110",
+          "101",
+          "101"
+      ],
+      "L": [
+          "10",
+          "10",
+          "10",
+          "11"
+      ],
+      "M": [
+          "11011",
+          "11011",
+          "10101",
+          "10001"
+      ],
+      "N": [
+          "1101",
+          "1101",
+          "1011",
+          "1001"
+      ],
+      "O": [
+          "111",
+          "101",
+          "101",
+          "111"
+      ],
+      "P": [
+          "111",
+          "101",
+          "111",
+          "100"
+      ],
+      "Q": [
+          "1110",
+          "1010",
+          "1110",
+          "0001"
+      ],
+      "R": [
+          "111",
+          "101",
+          "100",
+          "100"
+      ],
+      "S": [
+          "111",
+          "100",
+          "111",
+          "011"
+      ],
+      "T": [
+          "111",
+          "010",
+          "010",
+          "010"
+      ],
+      "U": [
+          "101",
+          "101",
+          "101",
+          "111"
+      ],
+      "V": [
+          "101",
+          "101",
+          "101",
+          "010"
+      ],
+      "W": [
+          "10001",
+          "10001",
+          "10101",
+          "01110"
+      ],
+      "X": [
+          "101",
+          "010",
+          "101",
+          "101"
+      ],
+      "Y": [
+          "101",
+          "101",
+          "010",
+          "010"
+      ],
+      "Z": [
+          "111",
+          "011",
+          "100",
+          "111"
+      ],
+      " ": [
+          "0",
+          "0",
+          "0",
+          "0"
+      ],
+      "!": [
+          "1",
+          "1",
+          "0",
+          "1"
+      ],
+      ".": [
+          "0",
+          "0",
+          "0",
+          "1"
+      ]
   };
 
-  this.clear = function(col) {
-    this.fillRect(0, 0, width, height, col);
-  }
+  window.framebuffer = function(width, height) {
+    // TODO(sww): Actually create + insert a canvas dynamically here.
 
-  this.rect = function(x, y, w, h, col) {
-    x = Math.floor(x);
-    y = Math.floor(y);
-    w = Math.floor(w);
-    h = Math.floor(h);
-    for(var i=x; i < x+w; i++) {
-      for(var j=y; j < y+h; j++) {
-        if(i < 0 || j < 0 || i >= width || j >= height) {
-          continue;
-        }
-        if(i == x || j == y || i == x+w-1 || j == y+h-1) {
-          this.px[i][j][0] = col[0];
-          this.px[i][j][1] = col[1];
-          this.px[i][j][2] = col[2];
-        }
-      }
-    }
-  };
-
-  this.fillCircle = function(x, y, r, col) {
-    x = Math.floor(x);
-    y = Math.floor(y);
-    r = Math.floor(r);
-    for(var i=-r; i <= r; i++) {
-      for(var j=-r; j <= r; j++) {
-        if(x+i < 0 || y+j < 0 || x+i >= width || y+j >= height) {
-          continue;
-        }
-        if(i*i + j*j <= r*r) {
-          px[x+i][y+j][0] = col[0];
-          px[x+i][y+j][1] = col[1];
-          px[x+i][y+j][2] = col[2];
-        }
-      }
-    }
-  };
-
-  this.circle = function(x, y, r, col) {
-    x = Math.floor(x);
-    y = Math.floor(y);
-    r = Math.floor(r);
-    for(var i=0; i < 360; i++) {
-      var cx = Math.round(Math.cos(i * (Math.PI / 180)) * r) + x;
-      var cy = Math.round(Math.sin(i * (Math.PI / 180)) * r) + y;
-
-      if(cx < 0 || cy < 0 || cx >= width || cy >= height) {
-        continue;
-      }
-      px[cx][cy][0] = col[0];
-      px[cx][cy][1] = col[1];
-      px[cx][cy][2] = col[2];
-    }
-  };
-
-  this.pixel = function(x, y, col) {
-    x = Math.floor(x);
-    y = Math.floor(y);
-
-    if(x < 0 || y < 0 || x >= width || y >= height) {
-      return;
-    }
-    px[x][y][0] = col[0];
-    px[x][y][1] = col[1];
-    px[x][y][2] = col[2];
-  };
-
-  this.shader = function(f) {
+    this.px = new Array(width);
     for(var i=0; i < width; i++) {
-      for(var j=0; j < height; j++) {
-        var result = f(i, j, px[i][j]);
-        px[i][j][0] = result[0];
-        px[i][j][1] = result[1];
-        px[i][j][2] = result[2];
+      this.px[i] = new Array(height);
+        for(var j=0; j < height; j++) {
+          this.px[i][j] = [ Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256) ];
+        }
+    }
+    var px = this.px;
+
+    this.width = width;
+    this.height = height;
+
+    this.fillRect = function(x, y, w, h, col) {
+      x = Math.floor(x);
+      y = Math.floor(y);
+      w = Math.floor(w);
+      h = Math.floor(h);
+      for(var i=x; i < x+w; i++) {
+        for(var j=y; j < y+h; j++) {
+          if(i < 0 || j < 0 || i >= width || j >= height) {
+            continue;
+          }
+          px[i][j][0] = col[0];
+          px[i][j][1] = col[1];
+          px[i][j][2] = col[2];
+        }
+      }
+    };
+
+    this.clear = function(col) {
+      this.fillRect(0, 0, width, height, col);
+    }
+
+    this.rect = function(x, y, w, h, col) {
+      x = Math.floor(x);
+      y = Math.floor(y);
+      w = Math.floor(w);
+      h = Math.floor(h);
+      for(var i=x; i < x+w; i++) {
+        for(var j=y; j < y+h; j++) {
+          if(i < 0 || j < 0 || i >= width || j >= height) {
+            continue;
+          }
+          if(i == x || j == y || i == x+w-1 || j == y+h-1) {
+            this.px[i][j][0] = col[0];
+            this.px[i][j][1] = col[1];
+            this.px[i][j][2] = col[2];
+          }
+        }
+      }
+    };
+
+    this.fillCircle = function(x, y, r, col) {
+      x = Math.floor(x);
+      y = Math.floor(y);
+      r = Math.floor(r);
+      for(var i=-r; i <= r; i++) {
+        for(var j=-r; j <= r; j++) {
+          if(x+i < 0 || y+j < 0 || x+i >= width || y+j >= height) {
+            continue;
+          }
+          if(i*i + j*j <= r*r) {
+            px[x+i][y+j][0] = col[0];
+            px[x+i][y+j][1] = col[1];
+            px[x+i][y+j][2] = col[2];
+          }
+        }
+      }
+    };
+
+    this.circle = function(x, y, r, col) {
+      x = Math.floor(x);
+      y = Math.floor(y);
+      r = Math.floor(r);
+      for(var i=0; i < 360; i++) {
+        var cx = Math.round(Math.cos(i * (Math.PI / 180)) * r) + x;
+        var cy = Math.round(Math.sin(i * (Math.PI / 180)) * r) + y;
+
+        if(cx < 0 || cy < 0 || cx >= width || cy >= height) {
+          continue;
+        }
+        px[cx][cy][0] = col[0];
+        px[cx][cy][1] = col[1];
+        px[cx][cy][2] = col[2];
+      }
+    };
+
+    this.pixel = function(x, y, col) {
+      x = Math.floor(x);
+      y = Math.floor(y);
+
+      if(x < 0 || y < 0 || x >= width || y >= height) {
+        return;
+      }
+      px[x][y][0] = col[0];
+      px[x][y][1] = col[1];
+      px[x][y][2] = col[2];
+    };
+
+    this.shader = function(f) {
+      for(var i=0; i < width; i++) {
+        for(var j=0; j < height; j++) {
+          var result = f(i, j, px[i][j]);
+          px[i][j][0] = result[0];
+          px[i][j][1] = result[1];
+          px[i][j][2] = result[2];
+        }
       }
     }
-  }
 
-  this.render = function() {
-    var canvas = document.getElementById('screen');
-    var ctx = canvas.getContext('2d');
+    var drawLetter = function(x, y, chr, rgb) {
+      var l = letters[chr.toUpperCase()];
+      if(!l) { return 0; }
+      for(var i=0; i < l[0].length; i++) {
+        for(var j=0; j < l.length; j++) {
+          if(l[j].charAt(i) === "1") {
+            $fb.pixel(x+i, y+j, rgb);
+          }
+        }
+      }
+      return l[0].length;
+    }
 
-    var pWidth = Math.floor(canvas.width / px.length);
-    var pHeight = Math.floor(canvas.height / px[0].length);
-    for(var i=0; i < px.length; i++) {
-      for(var j=0; j < px[i].length; j++) {
-        var r = Math.floor(px[i][j][0]);
-        var g = Math.floor(px[i][j][1]);
-        var b = Math.floor(px[i][j][2]);
-        ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-        ctx.fillRect(i*pWidth, j*pHeight, pWidth, pHeight);
+    this.text = function(x, y, txt, rgb) {
+      for(var i=0; i < txt.length; i++) {
+        x += drawLetter(x, y, txt.charAt(i), rgb) + 1;
       }
     }
-  }
 
-  return this;
-}
+    this.render = function() {
+      var canvas = document.getElementById('screen');
+      var ctx = canvas.getContext('2d');
+
+      var pWidth = Math.floor(canvas.width / px.length);
+      var pHeight = Math.floor(canvas.height / px[0].length);
+      for(var i=0; i < px.length; i++) {
+        for(var j=0; j < px[i].length; j++) {
+          var r = Math.floor(px[i][j][0]);
+          var g = Math.floor(px[i][j][1]);
+          var b = Math.floor(px[i][j][2]);
+          ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+          ctx.fillRect(i*pWidth, j*pHeight, pWidth, pHeight);
+        }
+      }
+    }
+
+    return this;
+  }
+})();
 
 function rand(max) {
   return Math.floor(Math.random() * max);
